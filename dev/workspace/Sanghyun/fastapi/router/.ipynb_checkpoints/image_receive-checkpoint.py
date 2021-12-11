618c33103a7633ca770d290fc9c11c5656391e51
Sang-hyun Park <hyun06000@gmail.com>
2021-12-11 10:43:42 +0000

    Modify backend server

--- dev/workspace/Sanghyun/fastapi/router/.ipynb_checkpoints/image_receive-checkpoint.py
+++ dev/workspace/Sanghyun/fastapi/router/.ipynb_checkpoints/image_receive-checkpoint.py
@@ -2,39 +2,62 @@ import sys
 import shutil
 from os import path as osp
 
-from PIL import Image
+from typing import List
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 import numpy as np
 
-from fastapi import APIRouter, File, UploadFile
-from starlette.responses import FileResponse
-from dl_model import test_pipe_fastapi as model
+import json
+import requests
+from pydantic import BaseModel
+
+from fastapi import APIRouter
 
 router = APIRouter(
     prefix="/upload",
     tags=["upload"]
 )
 
-BASE_DIR = osp.dirname(osp.dirname(osp.abspath(__file__)))
-IMG_DIR = osp.join(BASE_DIR, "asset/")
-SERVER_IMG_DIR = osp.join("http://localhost:8000/", "asset/")
+class Item(BaseModel):
+    # req = {
+    #     "image_as_list" : List # [H, W, C]
+    # }
+    image_as_list: List
 
-@router.post("/")
-async def image_file(
-    image : UploadFile = File(...)
+MODEL_URL = 'http://49.50.175.108:6010/items/'
+@router.post("/inference")
+async def image(
+    item : Item
 ):
-    if image is not None:
-        local_image = osp.join(IMG_DIR, image.filename)
-        with open(local_image, "wb") as buffer:
-            shutil.copyfileobj(image.file, buffer)
-        result = model.test(local_image)
-    
-    return {
-        "filename": image.filename,
-        "result" : result
+    image = item.image_as_list
+    # send to model server
+    req_dict = {
+        "instances" : image # [H, W, C]
     }
+    response = requests.post(
+        url=MODEL_URL,
+        data=json.dumps(req_dict)
+    )
+    
+    # parse a response
+    resp_eval = eval(response.text)
+    
+    im_shape = resp_eval["im_shape"]
+    degree_list = resp_eval["drg"]
+    group_list = resp_eval["grp"]
+    
+    fig, ax = plt.subplots()
+    ax.axis("off")
+    ax.imshow(np.array(image))
+    for group in group_list:
+        ax.plot(group[0], "*r", markersize = 15)
+        ax.plot(group[1], ".b", markersize = 20)
+        ax.plot(group[2], ".b", markersize = 20)
+    
+    fig_string = fig.canvas.to_string_rgb()
+    arr_image = np.fromstring(fig_string, dtype=np.int8, sep='')
+    arr_image = np.reshape(arr_image, im_shape)
+    
+    return {"im_plot" : arr_image.tolist()}
 
-
-
-@router.get("/asset/{filename}")
-async def image_show(filename: str):
-    return FileResponse(f"{IMG_DIR}/{filename}")
