a0e1b02915b9f839eaa7a5bf1f493f3e0dd4d49f
Sang-hyun Park <hyun06000@gmail.com>
2021-12-16 19:51:32 +0000

    Delete dev

--- dev/deploy/backend/docker_run.sh
+++
@@ -1,4 +0,0 @@
-docker run -d \
-    -p 8001:8001 \
-    --name backend \
-    backend-0.2.3
--- dev/deploy/model_server/.ipynb_checkpoints/run-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-conda activate DeepRuleFastAPI
-
-nohup uvicorn model_serve:app --reload --host 0.0.0.0 --port 6006 &
