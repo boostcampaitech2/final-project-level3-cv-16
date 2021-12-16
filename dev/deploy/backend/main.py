d7570a0765691a992e59c4096af3c504926f0522
Sang-hyun Park <hyun06000@gmail.com>
2021-12-16 13:37:02 +0000

    Add feature easyocr

--- dev/deploy/backend/main.py
+++ dev/deploy/backend/main.py
@@ -1,28 +1,37 @@
+import json
+import requests
 from typing import List
 
 import numpy as np
 
-import json
-import requests
-from pydantic import BaseModel
+import easyocr
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+from utils.ocr_utils import ocr_predict
 from utils.visualization import plot_model_results
 
-app = FastAPI() #REST API
+app = FastAPI()
 
-class Item(BaseModel):
-    # req = {
-    #     "image_as_list" : List # [H, W, C]
-    # }
-    image_as_list: List
+ocr_reader = []
+@app.on_event("startup")
+def set_ocr_reader():
+    reader = easyocr.Reader(['en'])
+    ocr_reader.append(reader)
 
-# MODEL_URL = 'http://49.50.175.108:6010/items/'
-MODEL_URL = 'http://sanghyun.ddns.net:8000/items/'
+
 
 @app.get("/")
 def main_page():
     return {"main":"page"}
+
+
+
+# MODEL_URL = 'http://49.50.175.108:6010/items/'
+MODEL_URL = 'http://sanghyun.ddns.net:8000/items/'
+
+class Item(BaseModel):
+    image_as_list: List # [H, W, C] : List[List[int, int, int]]
 
 @app.post("/backend/")
 async def create_item(
@@ -41,8 +50,10 @@ async def create_item(
     # parse a response from model server
     parsed_response = eval(response.text)
 
+    reader = ocr_reader[0]
+    ocr_predict(reader, image_arr, debug=False)
+
     visualised_image = plot_model_results(image_arr, parsed_response)
-    
     parsed_response.update({"im_plot" : visualised_image.tolist()})
 
     return parsed_response
