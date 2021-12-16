a36277586ea16d76411b2df6d61f9d205e225002
Sang-hyun Park <hyun06000@gmail.com>
2021-12-16 19:43:04 +0000

    Add feature ocr_result for frontend

--- dev/deploy/frontend/run.sh
+++ dev/deploy/frontend/run.sh
@@ -1,2 +1,2 @@
-nohup streamlit run main.py --server.address 0.0.0.0 --server.port 8501 &
+nohup streamlit run main.py --server.address 0.0.0.0 --server.port 8002 &
 echo "streamlit server is running"
